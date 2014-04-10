be239cacad3062d19f99f150305827270040c165
Phil Baines <phil.baines@wubbleyew.com>
2014-04-10 13:04:14 +0100

    Added a 'TestConsole' projec to test PDF feature

---
+++ src/TestConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using RbcTools.Library.Badges;
+
+namespace TestConsole
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Creating a PDF");
+			
+			var badges = new List<Badge>();
+			
+			for (int i = 0; i < 4; i++)
+			{
+				badges.Add(new Badge(){ FirstName = "John", LastName = "Doe", CongregationName = "Cardigan", DepartmentName = "Decorating" });
+				badges.Add(new Badge(){ FirstName = "Joe", LastName = "Bloggs", CongregationName = "Aberaeron", DepartmentName = "Air Handling" });
+				badges.Add(new Badge(){ FirstName = "Jane", LastName = "Doe", CongregationName = "Cardigan", DepartmentName = "Electricians" });
+				badges.Add(new Badge(){ FirstName = "Phil", LastName = "Baines", CongregationName = "Aberaeron", DepartmentName = "IT Department" });
+				badges.Add(new Badge(){ FirstName = "Madelyn", LastName = "Baines", CongregationName = "Aberaeron", DepartmentName = "Volunteers" });
+			}
+			
+			var builder = new BadgePdfBuilder(badges);
+			var fileName = builder.CreatePdf();
+			
+			var process = Process.Start(fileName);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
