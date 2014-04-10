95462d183aff795e0c71912a4abc5bdd5323e104
Phil Baines <phil.baines@wubbleyew.com>
2014-04-10 12:41:24 +0100

    Add a stub Badge class

---
+++ src/Library.Badges/Badge.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace RbcTools.Library.Badges
+{
+	public class Badge
+	{
+		public Badge(){}
+		
+		public string FirstName { get; set; }
+		
+		public string LastName { get; set; }
+		
+		public string CongregationName { get; set; }
+		
+		public string DepartmentName { get; set; }
+	}
+}
